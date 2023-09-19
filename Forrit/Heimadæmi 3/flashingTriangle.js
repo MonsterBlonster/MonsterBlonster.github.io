9355a9c4520488a2c38043174f6d11d0ccaf7c69
MonsterBlonster <binni.stefansson@gmail.com>
2023-09-19 21:50:02 +0000

    flashingTriangle

---
+++ Forrit/Heimadæmi 3/flashingTriangle.js
@@ -0,0 +1,60 @@
+///////////////////////////////////////////////////////////////////
+//    Sýnidæmi í Tölvugrafík
+//     Einfaldasta WebGL forritið.  Teiknar einn rauðan þríhyrning.
+//
+//    Hjálmtýr Hafsteinsson, ágúst 2023
+///////////////////////////////////////////////////////////////////
+var gl;
+var points;
+
+var locTime;
+var iniTime;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    var vertices = new Float32Array([-1, -1, 0, 1, 1, -1]);
+
+    //  Configure WebGL
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.95, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW );
+
+    // Associate shader variables with our data buffer
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    locTime = gl.getUniformLocation( program, "time" );
+
+    iniTime = Date.now();
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+
+    var msek = Date.now() - iniTime;
+    gl.uniform1f( locTime, msek );
+
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+
+    window.requestAnimFrame(render);
+}
